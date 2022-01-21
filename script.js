2ff92a69d0717f1bdf1810a04a6b52acd5d57df5
scias300 <scias300@gmail.com>
2022-01-21 22:23:37 +0300

    Created 1 player and 2 players with one easy difficulty yet

--- script.js
+++ script.js
@@ -1,52 +1,89 @@
 let cells = document.querySelectorAll('.cell');
 let overlay = document.querySelector('.overlay');
 let text = document.querySelector('.text');
-let start = document.querySelector('.start');
+let replay = document.querySelector('.replay');
 let lines = document.querySelectorAll('.line');
+let onePlayer = document.querySelector('.mode-1');
+let twoPlayer = document.querySelector('.mode-2');
+let menu = document.querySelector('.menu');
+let newGame = document.querySelector('.new-game');
+let container = document.querySelector('.container');
 let counter = 0;
 let tie = 0;
 let crosses = [];
 let zeros = [];
+let win = false;
+let vacants = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-start.addEventListener('click', startGame);
+onePlayer.addEventListener('click', startGameOne);
 
-cells.forEach((item, index) => {
-    item.setAttribute('num', index + 1);
-    item.addEventListener('click', makeMove);
+twoPlayer.addEventListener('click', startGameTwo);
+
+newGame.addEventListener('click', () => {
+    location.reload();
 });
 
-function startGame() {
+function startGameOne() {
+    createField();
+    cells.forEach(item => {
+        item.addEventListener('click', makeMoveAi);
+    });
+    container.classList.add('container-visible');
+    replay.addEventListener('click', createField);
+    replay.addEventListener('click', startGameOne);
+    menu.classList.add('hidden');
+}
+
+function startGameTwo() {
+    createField();
+    cells.forEach(item => {
+        item.addEventListener('click', makeMove);
+    });
+    container.classList.add('container-visible');
+    replay.addEventListener('click', createField);
+    replay.addEventListener('click', startGameTwo);
+    menu.classList.add('hidden');
+}
+
+function createField() {
+    cells.forEach((item, index) => {
+        item.setAttribute('num', index + 1);
+    });
     counter = 0;
     tie = 0;
     crosses = [];
     zeros = [];
+    vacants = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     cells.forEach(item => {
         item.classList.remove('cross');
         item.classList.remove('zero');
-        item.addEventListener('click', makeMove);
     });
     overlay.classList.remove('visible');
     text.textContent = '';
     lines.forEach(item => {
         item.classList.remove('visible');
     });
+    win = false;
 }
 
 function makeMove(event) {
+    let num = +this.getAttribute('num');
     if (counter == 0) {
         event.target.classList.add('cross');
-        crosses.push(+this.getAttribute('num'));
+        crosses.push(num);
         counter++;
         event.target.removeEventListener('click', makeMove);
+        vacants = vacants.filter((item) => item != num);
         isTie();
         isWin();
         return;
     }
     if (counter == 1) {
         event.target.classList.add('zero');
-        zeros.push(+this.getAttribute('num'));
+        zeros.push(num);
         counter--;
         event.target.removeEventListener('click', makeMove);
+        vacants = vacants.filter((item) => item != num);
         isTie();
         isWin();
         return;
@@ -68,13 +105,33 @@ function isWin() {
             overlay.classList.add('visible');
             text.textContent = 'Cross Win';
             document.querySelector(`.line-${index + 1}`).classList.add('visible');
-            console.log(document.querySelector(`.line-${index + 1}`));
+            win = true;
         }
         if (zeros.includes(item[0]) && zeros.includes(item[1]) && zeros.includes(item[2])) {
             overlay.classList.add('visible');
             text.textContent = 'Zero Win';
             document.querySelector(`.line-${index + 1}`).classList.add('visible');
-            console.log(index + 1);
+            win = true;
         }
     });
+}
+
+function makeMoveAi(event) {
+    let num = +this.getAttribute('num');
+    event.target.classList.add('cross');
+    crosses.push(num);
+    event.target.removeEventListener('click', makeMoveAi);
+    vacants = vacants.filter((item) => item != num);
+    isTie();
+    isWin();
+    if (win === false && tie < 9) {
+        let random = vacants[Math.floor(Math.random() * vacants.length)];
+        let randomElem = document.querySelector(`.cell-${random}`);
+        randomElem.classList.add('zero');
+        zeros.push(random);
+        randomElem.removeEventListener('click', makeMoveAi);
+        vacants = vacants.filter((item) => item != random);
+        isTie();
+        isWin();
+    }
 }

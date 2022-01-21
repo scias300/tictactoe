d504ef0ff334d9dc94f20b2e3f6807752a6a91b5
scias300 <scias300@gmail.com>
2022-01-21 17:54:39 +0300

    Added lines for wins

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let cells = document.querySelectorAll('.cell');
 let overlay = document.querySelector('.overlay');
 let text = document.querySelector('.text');
 let start = document.querySelector('.start');
+let lines = document.querySelectorAll('.line');
 let counter = 0;
 let tie = 0;
 let crosses = [];
@@ -26,6 +27,9 @@ function startGame() {
     });
     overlay.classList.remove('visible');
     text.textContent = '';
+    lines.forEach(item => {
+        item.classList.remove('visible');
+    });
 }
 
 function makeMove(event) {
@@ -59,14 +63,18 @@ function isTie() {
 
 function isWin() {
     const wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
-    wins.forEach(item => {
+    wins.forEach((item, index) => {
         if (crosses.includes(item[0]) && crosses.includes(item[1]) && crosses.includes(item[2])) {
             overlay.classList.add('visible');
             text.textContent = 'Cross Win';
+            document.querySelector(`.line-${index + 1}`).classList.add('visible');
+            console.log(document.querySelector(`.line-${index + 1}`));
         }
         if (zeros.includes(item[0]) && zeros.includes(item[1]) && zeros.includes(item[2])) {
             overlay.classList.add('visible');
             text.textContent = 'Zero Win';
+            document.querySelector(`.line-${index + 1}`).classList.add('visible');
+            console.log(index + 1);
         }
     });
 }
